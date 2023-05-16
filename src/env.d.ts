89ac38875d00bc817ecec9f1f7ae92f812ebb0b4
jheehg <juhee.hg@gmail.com>
2023-05-16 17:47:33 +0900

    chore: add supabase

---
+++ src/env.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="node" />
+
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv {
+			readonly SUPABASE_APP: string;
+			readonly SUPABASE_ANON_KEY: string;
+		}
+	}
+}
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export {};
