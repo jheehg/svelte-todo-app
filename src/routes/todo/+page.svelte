9d3d2bb1d139b0969195f2f65a6c32dad594b1dc
jheehg <juhee.hg@gmail.com>
2023-05-23 18:23:06 +0900

    feat: add custom event to call api

--- src/routes/todo/+page.svelte
+++ src/routes/todo/+page.svelte
@@ -1,24 +1,66 @@
-<script>
+<script lang="ts">
 	import InputForm from '$lib/components/InputForm.svelte';
 	import Modal from '$lib/components/Modal.svelte';
 	import TodoList from '$lib/components/TodoList.svelte';
 	import { isModalOpen, list } from '$lib/stores';
+	import type { ITodoItem, Status } from '$lib/types';
+	import { fetchTodo } from '$lib/utils/api';
+	import { onMount } from 'svelte';
 
-	$: upcomingList = $list.filter((item) => item.status === 'upcoming');
-	$: progressList = $list.filter((item) => item.status === 'progress');
-	$: doneList = $list.filter((item) => item.status === 'done');
+	export let data: { todo: ITodoItem[] };
+
+	// let { todo } = data;
+	// $: ({ todo } = data);
+
+	onMount(() => {
+		list.set(data?.todo);
+	});
+
+	let todoInfo: {
+		list: ITodoItem[];
+		statusType: Status;
+		status: string;
+	}[];
+
+	$: {
+		const upcomingList = $list.filter((item) => item.status === 'upcoming');
+		const progressList = $list.filter((item) => item.status === 'progress');
+		const doneList = $list.filter((item) => item.status === 'done');
+
+		todoInfo = [
+			{
+				list: upcomingList,
+				statusType: 'upcoming',
+				status: 'Upcoming'
+			},
+			{
+				list: progressList,
+				statusType: 'progress',
+				status: 'In Progress'
+			},
+			{
+				list: doneList,
+				statusType: 'done',
+				status: 'Done'
+			}
+		];
+	}
 </script>
 
 <div class="container px-4 mx-auto mt-10">
 	<div class="max-md:grid md:flex flex-row justify-center">
-		<TodoList data={upcomingList} status="Upcoming" />
-		<TodoList data={progressList} status="Progress" />
-		<TodoList data={doneList} status="Done" />
+		{#each todoInfo as info}
+			<TodoList
+				data={{ list: info.list, statusType: info.statusType }}
+				status={info.status}
+				on:fetchTodo={fetchTodo}
+			/>
+		{/each}
 	</div>
 </div>
 
 {#if $isModalOpen}
 	<Modal>
-		<InputForm />
+		<InputForm on:fetchTodo={fetchTodo} />
 	</Modal>
 {/if}
