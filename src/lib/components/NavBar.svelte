1aaff00349ac4d8a2bf8ecd61a999a8589a8e9dc
jheehg <juhee.hg@gmail.com>
2023-05-26 17:11:52 +0900

    feat: add NavBar component

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { page } from '$app/stores';
+
+	const signUpUser = async () => {
+		const supabase = $page.data.supabase;
+		const { error } = await supabase.auth.signOut();
+	};
+</script>
+
+<div class="navbar bg-base-100 border-y">
+	<div class="flex-1" />
+	<div class="flex-none">
+		<ul class="menu menu-horizontal px-5">
+			<li><a href="#" on:click={signUpUser}>Sign Out</a></li>
+		</ul>
+	</div>
+</div>
