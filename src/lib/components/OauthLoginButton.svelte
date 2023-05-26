1d0a22652776105db9d29d9eb89829ef1a72ce33
jheehg <juhee.hg@gmail.com>
2023-05-26 17:09:47 +0900

    feat: add OauthLoginButton component

---
+++ src/lib/components/OauthLoginButton.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { page } from '$app/stores';
+
+	const signInWithGoogle = async () => {
+		const supabase = $page.data.supabase;
+		const { error } = await supabase.auth.signInWithOAuth({
+			provider: 'google'
+		});
+	};
+</script>
+
+<button on:click={signInWithGoogle} class="font-seimbold">Continue with Google</button>
